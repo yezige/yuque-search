70f90ee5b15308324c2fc1d21cff284613b5fe87
yezi <a@liu.app>
2023-06-05 09:32:13 +0800

    更新gtag

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // 首次安装事件
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.openOptionsPage(() => {
-    console.log('安装后打开选项页')
-  })
+  // chrome.runtime.openOptionsPage(() => {
+  //   console.log('安装后打开选项页')
+  // })
 })
